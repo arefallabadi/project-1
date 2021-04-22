6754846e1bb008544ee00822e5db4271c99dd87a
arefallabadi <labadi.aref@gmail.com>
2021-04-23 00:38:31 +0300

    change style & add first function

--- main.js
+++ main.js
@@ -1,16 +1,26 @@
 let Q =[
-{q1 : a},
-{q2 : b},
-{q3 : c},
-{q4 : d},
-{q5 : e},
-{q6 : f},
-{q7 : g},
-{q8 : h},
-{q9 : i},
-{q10:j},
+{q1 : "a"},
+{q2 : "b"},
+{q3 : "c"},
+{q4 : "d"},
+{q5 : "e"},
+{q6 : "f"},
+{q7 : "g"},
+{q8 : "h"},
+{q9 : "i"},
+{q10: "j"},
 ]
 
-const play= ()=> {
-    
-}
+const q = document.querySelector("#q") 
+const a = document.querySelector("#a")
+const b = document.querySelector("#b")
+const c = document.querySelector("#c")
+const d = document.querySelector("#d")
+
+  const play= (Q)=> {
+  for(let i =0 ; i < Q.length ; i++ ){
+      q.innerHTML=Object.keys(Q[i])
+      a.innerHTML=Q[i][Object.keys(Q[i])]
+  }
+ }
+ 
